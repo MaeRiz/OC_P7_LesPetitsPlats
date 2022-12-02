f5066edb2cb401c0f32d9caaefb3d6711825d19b
Mahery Gonin <mahery.gonin@gmail.com>
2022-12-02 13:25:44 +0100

    add search tag system, add ustensils & appliances

--- js/index.js
+++ js/index.js
@@ -16,36 +16,91 @@ function getIngredients() {
     }));
 }
 
+function getAppliances() {
+    let appliancesList = []
+
+    for(let i = 0; i < recipes.length; i++){
+        if(!appliancesList.includes(recipes[i].appliance)) {
+            appliancesList.push(recipes[i].appliance);
+        }
+    }
+    return (appliancesList.sort(function (a, b) {
+        if (a < b) {return -1;};
+        if (a > b) {return 1;};
+        return 0;
+    }));
+}
+
+function getUstensils() {
+    let ustensilsList = []
+
+    for(let i = 0; i < recipes.length; i++){
+        for(ustensil of recipes[i].ustensils) {
+            if(!ustensilsList.includes(ustensil)) {
+                ustensilsList.push(ustensil);
+            }
+        }
+    }
+
+    return (ustensilsList.sort(function (a, b) {
+        if (a < b) {return -1;};
+        if (a > b) {return 1;};
+        return 0;
+    }));
+}
+
 function createDomTags(list, section) {
     let i = 0
     for(tag of list) {
         let item = document.createElement('a');
-        item.setAttribute('onclick', 'selectTag(this)')
+        item.setAttribute('onclick', 'selectTag(this)');
         item.classList.add('dropdown-item');
-        item.id = section.parentElement.id + '_' + i
+        item.id = section.parentElement.id + '_' + i;
         item.textContent = tag;
         section.appendChild(item);
-        i++
+        i++;
     }
 }
 
 function selectTag(element) {
     const tagSection = document.querySelector('#tags');
+    const searchTagBar = element.parentElement.parentElement.childNodes[3];
+    searchTagBar.focus();
 
     const tag = document.createElement('a');
+    console.log(element.parentElement.parentElement)
+    tag.classList.add(element.parentElement.parentElement.id + "_tag");
     tag.innerHTML = element.innerText + '<i class="fa-regular fa-circle-xmark" onclick="removeTag(this, ' + element.id +')"></i>';
     tagSection.appendChild(tag);
-    element.style.display = "none";
+    element.classList.add('selected');
 }
 
 function removeTag(element, tagid) {
     tagid.style.display = "block";
+    tagid.classList.remove('selected');
     element.parentElement.remove();
 }
 
+function searchTag(element) {
+    filter = element.value.toUpperCase();
+    a = element.nextSibling.nextSibling.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (!a[i].classList.contains("selected")) {
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+          } else {
+            a[i].style.display = "none";
+        }
+      }
+    }
+  } 
+
 function init () {
     /* Create DOM items for ingredient; param 1: get ingredients list, param 2 section queryselected*/
-    createDomTags(getIngredients(), document.querySelector(".ingredients-list"));
-}
+    createDomTags(getIngredients(), document.querySelector("#ingredients-list"));
+    createDomTags(getAppliances(), document.querySelector("#appliances-list"));
+    createDomTags(getUstensils(), document.querySelector("#ustensils-list"));
+}   
 
 init();
